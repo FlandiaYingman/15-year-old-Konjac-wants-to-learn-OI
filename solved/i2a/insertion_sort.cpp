aad11b602b409ee7600ac83ae25c1ce9eac485cf
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-07-18 15:54:06 +0800

    format(i2a): insertion sort, merge sort

--- solved/i2a/insertion_sort.cpp
+++ solved/i2a/insertion_sort.cpp
@@ -5,43 +5,43 @@
 using namespace std;
 
 void insertion_sort(int len, int *arr) {
-  for (size_t i = 0; i < len; i++) {
-    for (size_t j = 0; j <= i; j++) {
-      if (arr[i] < arr[j]) {
-        int temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      }
+    for (size_t i = 0; i < len; i++) {
+        for (size_t j = 0; j <= i; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
     }
-  }
 }
 
 void insertion_sort_i2a(int len, int *arr) {
-  for (size_t j = 1; j < len; j++) {
-    int key = arr[j];
-    size_t i = j - 1;
-    while (i >= 0 && arr[i] > key) {
-      arr[i + i] = arr[i];
-      i--;
+    for (size_t j = 1; j < len; j++) {
+        int key = arr[j];
+        size_t i = j - 1;
+        while (i >= 0 && arr[i] > key) {
+            arr[i + i] = arr[i];
+            i--;
+        }
+        arr[i + 1] = key;
     }
-    arr[i + 1] = key;
-  }
 }
 
 int main() {
-  int len;
-  int arr[100000];
+    int len;
+    int arr[100000];
 
-  cin >> len;
-  for (size_t i = 0; i < len; i++) {
-    cin >> arr[i];
-  }
+    cin >> len;
+    for (size_t i = 0; i < len; i++) {
+        cin >> arr[i];
+    }
 
-  insertion_sort(len, arr);
+    insertion_sort(len, arr);
 
-  for (size_t i = 0; i < len; i++) {
-    cout << arr[i] << " ";
-  }
+    for (size_t i = 0; i < len; i++) {
+        cout << arr[i] << " ";
+    }
 
-  return 0;
+    return 0;
 }
